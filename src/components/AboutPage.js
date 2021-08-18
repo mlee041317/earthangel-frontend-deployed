08d5cb90a4238d5d0a64a0b01a3446ccef27830a
mlee041317 <michelleleefit@gmail.com>
2021-08-17 18:02:51 -0700

    testing

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -8,7 +8,7 @@ const About = () => {
         <Card.Body>
           <Card.Title><h2>about | earthangel</h2></Card.Title>
             <Card.Text>
-            The social media ascendance of mystics, angels, and the #EarthAngelofInstagram is one of the greatest uses of Internet of our time — period. One of the most enchanting of them all is Michelle — also know as @theearthangel — who delivers a heady mix of astrology knowledge, mystical visuals, self-care wisdom, and some legit #bigangelenergy on her website. 
+            The social media ascendance of mystics, angels, and the #EarthAngelofInstagram is one of the greatest uses of Internet of our time — period. One of the most enchanting of them all is Michelle — also know as @theearthangel — who delivers a heady mix of astrology knowledge, mystical visuals, self-care wisdom, and some legit #bigangelenergy on her website!
             </Card.Text>
         </Card.Body>
     </Card>
